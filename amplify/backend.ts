c36f3530a0891264e32ac0a71d3f3078c5032085
alex <Alex>
2025-10-26 21:10:37 +0100

    wip

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,5 +1,5 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { email } from './email/resource';
+import { email } from './functions/email/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
