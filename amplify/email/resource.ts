313dbb8a6add34c4afef182804e6c850be08e176
alex <Alex>
2025-10-26 20:53:31 +0100

    wip

--- amplify/email/resource.ts
+++ amplify/email/resource.ts
@@ -1,16 +1,25 @@
 import { defineFunction } from '@aws-amplify/backend';
-import 'dotenv/config';
+// import 'dotenv/config';
 
 /**
  * Define and configure your email resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
+// @ts-ignore
 export const email = defineFunction({
+// @ts-ignore
   name: "email",
+// @ts-ignore
   entry: "./handler.ts",
+// @ts-ignore
+//   environment: {
+//     SMTP_USERNAME: process.env.SMTP_USERNAME,
+//     SMTP_PASS: process.env.SMTP_PASS,
+//     MAIL_TO: process.env.MAIL_TO,
+//   },
   environment: {
-    SMTP_USERNAME: process.env.SMTP_USERNAME,
-    SMTP_PASS: process.env.SMTP_PASS,
-    MAIL_TO: process.env.MAIL_TO,
+    SMTP_USERNAME: 'sashakypinko@gmail.com',
+    SMTP_PASS: 'yqib fvxv nlnm doln',
+    MAIL_TO: 'sashakypinko@gmail.com',
   },
 });
